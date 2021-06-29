7274891a4336bc1c9db56eacff266aa3b3e8f9d8
keigo <k5.trismegistus@gmail.com>
2021-06-29 22:44:09 +0900

    :sparkle: Add error page

---
+++ frontend/pages/error.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-container>
+    <v-container>
+      <v-row justify="center">
+        <v-col cols="12" align="center">
+          <nuxt-link :to="{ name: 'index' }">
+            <v-img
+              class="link"
+              contain
+              max-width="120"
+              src="/logo_transparent.png"
+            ></v-img>
+          </nuxt-link>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col cols="12">
+          <h1>Error</h1>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col>
+          <h2>Something wrong with this site</h2>
+          <p>
+            If you'd like, please submit bug report as issue <a class="here-link" href="https://github.com/k5trismegistus/nexxtrack" target="_blank">here</a> (Jump to Github)
+          </p>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-container>
+</template>
+<script>
+export default {
+  head: { title: 'Error' },
+}
+</script>
+<style scoped>
+.here-link {
+  color: white;
+  text-decoration: underline;
+}
+</style>
